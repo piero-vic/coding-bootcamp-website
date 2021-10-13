ea5329171cb06a6d7e01e6714984cf15d5559d70
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-10-13 17:06:11 -0500

    Add speakers objects

--- index.js
+++ index.js
@@ -5,3 +5,42 @@ function toggleNavMenu() {
 
 const toggleButtons = Array.from(document.getElementsByClassName('toggle-btn'));
 toggleButtons.forEach((element) => element.addEventListener('click', toggleNavMenu));
+
+const speakers = [
+  {
+    name: 'Stanley Washington',
+    position: 'Marketing Coordinator',
+    summary: 'Lorem ipsum dolor sit amet sed do eiusmod tempor incididunt magna aliqua.',
+    photo: 'https://randomuser.me/api/portraits/men/47.jpg',
+  },
+  {
+    name: 'Ruby Henry',
+    position: 'Sales Representative',
+    summary: 'Lorem ipsum dolor sit amet sed do eiusmod tempor incididunt magna aliqua.',
+    photo: 'https://randomuser.me/api/portraits/women/30.jpg',
+  },
+  {
+    name: 'Gina Fisher',
+    position: 'Financial Analyst',
+    summary: 'Lorem ipsum dolor sit amet sed do eiusmod tempor incididunt magna aliqua.',
+    photo: 'https://randomuser.me/api/portraits/women/47.jpg',
+  },
+  {
+    name: 'Kelly Ward',
+    position: 'Marketing Coordinator',
+    summary: 'Lorem ipsum dolor sit amet sed do eiusmod tempor incididunt magna aliqua.',
+    photo: 'https://randomuser.me/api/portraits/men/30.jpg',
+  },
+  {
+    name: 'Abigail Ramirez',
+    position: 'Sales Representative',
+    summary: 'Lorem ipsum dolor sit amet sed do eiusmod tempor incididunt magna aliqua.',
+    photo: 'https://randomuser.me/api/portraits/women/23.jpg',
+  },
+  {
+    name: 'Walter Owens',
+    position: 'Financial Analyst',
+    summary: 'Lorem ipsum dolor sit amet sed do eiusmod tempor incididunt magna aliqua.',
+    photo: 'https://randomuser.me/api/portraits/men/22.jpg',
+  },
+];
