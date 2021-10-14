ddf71aefc9e614b98b4991203ab9ab41ee4818f1
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-10-14 13:07:57 -0500

    Edit placeholder text

--- index.js
+++ index.js
@@ -25,37 +25,37 @@ toggleButtons.forEach((element) => element.addEventListener('click', toggleNavMe
 const speakers = [
   {
     name: 'Stanley Washington',
-    position: 'Marketing Coordinator',
+    position: 'Full Stack Software Developer',
     summary: 'Lorem ipsum dolor sit amet sed do eiusmod tempor incididunt magna aliqua.',
     photo: 'https://randomuser.me/api/portraits/men/47.jpg',
   },
   {
     name: 'Ruby Henry',
-    position: 'Sales Representative',
+    position: 'Font End Developer / Graphic Designer',
     summary: 'Lorem ipsum dolor sit amet sed do eiusmod tempor incididunt magna aliqua.',
     photo: 'https://randomuser.me/api/portraits/women/30.jpg',
   },
   {
     name: 'Gina Fisher',
-    position: 'Financial Analyst',
+    position: 'Blockchain Developer',
     summary: 'Lorem ipsum dolor sit amet sed do eiusmod tempor incididunt magna aliqua.',
     photo: 'https://randomuser.me/api/portraits/women/47.jpg',
   },
   {
     name: 'Kelly Ward',
-    position: 'Marketing Coordinator',
+    position: 'Cyber Security Expert',
     summary: 'Lorem ipsum dolor sit amet sed do eiusmod tempor incididunt magna aliqua.',
     photo: 'https://randomuser.me/api/portraits/men/30.jpg',
   },
   {
     name: 'Abigail Ramirez',
-    position: 'Sales Representative',
+    position: 'Financial Analyst',
     summary: 'Lorem ipsum dolor sit amet sed do eiusmod tempor incididunt magna aliqua.',
     photo: 'https://randomuser.me/api/portraits/women/23.jpg',
   },
   {
     name: 'Walter Owens',
-    position: 'Financial Analyst',
+    position: 'Data Scientist',
     summary: 'Lorem ipsum dolor sit amet sed do eiusmod tempor incididunt magna aliqua.',
     photo: 'https://randomuser.me/api/portraits/men/22.jpg',
   },
